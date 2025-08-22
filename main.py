7d9bd48efecb0c0d0e20176d8e3a7cb23d2d175d
Jhon-Mota <jhonathan.nasc21@gmail.com>
2025-08-21 21:26:28 -0300

    Graph functionality finished.

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import pandas as pd
+import seaborn as sea
+import matplotlib.pyplot as plt
 
 df = pd.read_excel("")
 
 df.dropna(subset = ["Id Loja", "Id Produto"])
 
-sales = df.groupby("Id Loja")["Qtd. Vendida"].sum().sort_values(ascending = False).head()
+sales = df.groupby("Id Loja")["Qtd. Vendida"].sum().head()
 
-print(sales)
+sales_df = sales.reset_index()
+
+
+sea.set_theme(style="darkgrid")
+sea.barplot(data=sales_df, x="Id Loja", y="Qtd. Vendida")
+
+plt.show()
+
+#print(sales)
