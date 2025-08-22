c06a463f1db3162438db991a269729c85c268fb2
Jhon-Mota <jhonathan.nasc21@gmail.com>
2025-08-21 22:13:54 -0300

    Problems with the graphic fixed.

--- main.py
+++ main.py
@@ -2,18 +2,22 @@ import pandas as pd
 import seaborn as sea
 import matplotlib.pyplot as plt
 
+
 df = pd.read_excel("")
 
 df.dropna(subset = ["Id Loja", "Id Produto"])
 
 sales = df.groupby("Id Loja")["Qtd. Vendida"].sum().head()
+sales_sorted = sales.sort_values(ascending = False)
 
-sales_df = sales.reset_index()
+sales_df = sales_sorted.reset_index()
 
 
 sea.set_theme(style="darkgrid")
 sea.barplot(data=sales_df, x="Id Loja", y="Qtd. Vendida")
 
 plt.show()
+
+
 
 #print(sales)
